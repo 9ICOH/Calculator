6191de3996fab07bfa88b08a3f439b8635d8a41a
Jaroslav <9icoh@mail.ru>
2014-12-09 17:48:04 +0200

    Changes in view.

--- Calculator/Controllers/CalculatorController.cs
+++ Calculator/Controllers/CalculatorController.cs
@@ -42,78 +42,6 @@ namespace Calculator.Controllers
             return this.View(this.resultVm);
         }
 
-        public ActionResult SetSymbol(string submitButton)
-        {
-            if (this.Session[ModelName] != null)
-            {
-                this.resultVm = (ResultViewModel)this.Session[ModelName];
-            }
-
-            switch (submitButton)
-            {
-                case "(":
-                case ")":
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "0":
-                case "+":
-                case "-":
-                case "*":
-                case "/":
-                case ".":
-                    if (this.resultVm.OutputLine == "Error" | this.resultVm.OutputLine == "0" | this.resultVm.OutputLine == this.resultVm.LastCountedResult)
-                    {
-                        this.resultVm.OutputLine = string.Empty;
-                    }
-
-                    this.resultVm.OutputLine += submitButton;
-                    this.resultVm.LastExpression = this.resultVm.OutputLine;
-                    this.Session[ModelName] = this.resultVm;
-                    return this.View("Calculator", this.resultVm);
-                case "=":
-                    try
-                    {
-                        var calcMethod = this.opService.Compile<Calculate>(this.resultVm.OutputLine) as Calculate;
-                        if (calcMethod != null)
-                        {
-                            this.resultVm.LastCountedResult = calcMethod();
-                        }
-
-                        this.opService.Create(this.resultVm.OutputLine, this.resultVm.LastCountedResult);
-                        this.resultVm.OutputLine = this.resultVm.LastCountedResult;
-                        //this.resultVm.OperationsHistory = this.opService.All();
-                    }
-                    catch (Exception ex)
-                    {
-                        this.resultVm.OutputLine = ex.Message;
-                    }
-
-                    return this.View("Calculator", this.resultVm);
-                case "DEL":
-                    var charArray = this.resultVm.OutputLine.ToArray();
-                    var length = charArray.Length;
-
-                    return this.View("Calculator", this.resultVm);
-                case "AC":
-                    this.resultVm = new ResultViewModel
-                    {
-                        OutputLine = "0",
-                        LastExpression = "0"
-                    };
-                    Session.RemoveAll();
-                    return this.View("Calculator", this.resultVm);
-                default:
-                    return this.View("Calculator", this.resultVm);
-            }
-        }
-
         [HttpPost]
         public string GetResult(string expression)
         {
@@ -136,6 +64,7 @@ namespace Calculator.Controllers
             }
         }
 
+        [HttpPost]
         public ActionResult GetOperations()
         {
             return this.PartialView("HistoryOperationsView", this.opService.All());
